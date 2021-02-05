a76715602d820f508ced397750eacb15b9cbac10
SHughesCoe <shughes@coe.edu>
2021-02-05 11:32:28 -0600

    Initial commit

---
+++ app/src/main/java/edu/coe/hughes/myapplicationU/MainActivity.kt
@@ -0,0 +1,14 @@
+package edu.coe.hughes.myapplicationU
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cclayout)
+
+
+    }
+}
